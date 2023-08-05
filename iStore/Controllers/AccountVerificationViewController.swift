678ab3a22e0ca6a1abcc24237df104ef956c800b
islam.bouayache <islam.bouayache@itcomp-dz.com>
2023-08-06 00:49:00 +0100

    first commit

---
+++ iStore/Controllers/AccountVerificationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AccountVerificationViewController.swift
+//  iStore
+//
+//  Created by Mohamed Islam BOUAYACHE on 5/8/2023.
+//
+
+import UIKit
+
+class AccountVerificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iStore/Views/CategoryViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryViewCell.swift
+//  iStore
+//
+//  Created by Mohamed Islam BOUAYACHE on 5/8/2023.
+//
+
+import UIKit
+
+class CategoryViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
